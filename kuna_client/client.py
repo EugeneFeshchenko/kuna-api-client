89e6ea9b55067a7c62d16ff1b5395babb09daf81
Eugene Feshchenko <eugene@cybeready.com>
2020-03-13 18:31:45 +0200

    Implement calls to all endpoints

--- kuna_client/client.py
+++ kuna_client/client.py
@@ -1,19 +1,23 @@
 import hashlib
 import hmac
 import json
+import os
 import time
 from collections import OrderedDict
 from urllib.parse import urlencode
-import os
 
 import requests
 
 
 class KunaApiClient:
     API_DOMAIN = 'https://kuna.io/api/v2'
-    TICKERS_URL = f'{API_DOMAIN}/tickers/ethuah'
+    TIMESTAMP_URL = f'{API_DOMAIN}/timestamp'
+    TICKERS_URL = f'{API_DOMAIN}/tickers/'
+    ORDER_BOOK_URL = f'{API_DOMAIN}/depth'
+    TRADES_HISTORY_URL = f'{API_DOMAIN}/trades'
+
     ORDERS_URL = f'{API_DOMAIN}/orders'
-    ORDER_DELETE_URL = f'{API_DOMAIN}/order/delete'
+    CANCEL_ORDER_URL = f'{API_DOMAIN}/order/delete'
     TRADES_URL = f'{API_DOMAIN}/trades/my'
     ME_URL = f'{API_DOMAIN}/members/me'
 
@@ -31,68 +35,54 @@ class KunaApiClient:
         signature = signature.hexdigest()
         return f'{url}?access_key={self.api_key}&tonce={tonce}&signature={signature}'
 
-    def sell_eth(self, volume, price):
-        params = {'side': 'sell',
-                  'volume': volume,
-                  'market': 'ethuah',
-                  'price': price}
-        url = self.__build_personal_url(self.ORDERS_URL, 'POST', params)
-        r = requests.post(url, params)
-        return r
-
-    def buy_eth(self, volume, price):
-        params = {'side': 'buy',
-                  'volume': volume,
-                  'market': 'ethuah',
-                  'price': price}
-        url = self.__build_personal_url(self.ORDERS_URL, 'POST', params)
-        r = requests.post(url, params)
-        return r
-
-    def get_currency_balance(self, currency_name):
-        r = requests.get(self.__build_personal_url(self.ME_URL, 'GET', {}))
-        r.raise_for_status()
-        r = json.loads(r.content.decode('utf-8'))
-        return [x for x in r['accounts'] if x['currency'] == currency_name][0]['balance']
-
-    def get_balance(self):
-        r = requests.get(self.__build_personal_url(self.ME_URL, 'GET', {}))
-        r.raise_for_status()
-        r = json.loads(r.content.decode('utf-8'))
-        return r['accounts']
-
-    def get_eth_sell_rate(self):
-        r = requests.get(self.TICKERS_URL)
-        return float(json.loads(r.content.decode())['ticker']['sell'])
-
-    def get_eth_buy_rate(self):
-        r = requests.get(self.TICKERS_URL)
-        return float(json.loads(r.content.decode())['ticker']['buy'])
-
-    def get_tick(self):
-        r = requests.get(self.TICKERS_URL)
+    def timestamp(self):
+        r = requests.get(self.TIMESTAMP_URL)
         r.raise_for_status()
         return json.loads(r.content.decode())
 
-    def get_active_orders(self, side=None):
-        params = {'market': 'ethuah'}
-        url = self.__build_personal_url(self.ORDERS_URL, 'GET', params)
-        r = requests.get(url, params)
+    def tick(self, market):
+        r = requests.get(f'{self.TICKERS_URL}/{market}')
         r.raise_for_status()
-        r = json.loads(r.content.decode('utf-8'))
-        if side:
-            r = [x for x in r if x['side'] == side]
-        return r
+        return json.loads(r.content.decode())
 
-    def get_trades_history(self):
-        params = {'market': 'ethuah'}
-        url = self.__build_personal_url(self.TRADES_URL, 'GET', params)
-        r = requests.get(url, params)
+    def order_book(self, market):
+        r = requests.get(f'{self.ORDER_BOOK_URL}/?market={market}')
         r.raise_for_status()
-        r = json.loads(r.content.decode('utf-8'))
-        return r
+        return json.loads(r.content.decode())
+
+    def trades_history(self, market):
+        r = requests.get(f'{self.TRADES_HISTORY_URL}/?market={market}')
+        r.raise_for_status()
+        return json.loads(r.content.decode())
+
+    def me(self):
+        r = requests.get(self.__build_personal_url(self.ME_URL, 'GET', {}))
+        r.raise_for_status()
+        return json.loads(r.content.decode('utf-8'))
+
+    def place_order(self, side, volume, price, market):
+        params = {'side': side,
+                  'volume': volume,
+                  'market': market,
+                  'price': price}
+        url = self.__build_personal_url(self.ORDERS_URL, 'POST', params)
+        return requests.post(url, params)
 
     def cancel_order(self, order_id):
         params = {'id': order_id}
-        url = self.__build_personal_url(self.ORDER_DELETE_URL, 'POST', params)
+        url = self.__build_personal_url(self.CANCEL_ORDER_URL, 'POST', params)
         return requests.post(url, params)
+
+    def active_orders(self, market):
+        params = {'market': market}
+        url = self.__build_personal_url(self.ORDERS_URL, 'GET', params)
+        r = requests.get(url, params)
+        r.raise_for_status()
+        return json.loads(r.content.decode('utf-8'))
+
+    def my_trades_history(self, market):
+        params = {'market': market}
+        url = self.__build_personal_url(self.TRADES_URL, 'GET', params)
+        r = requests.get(url, params)
+        r.raise_for_status()
+        return json.loads(r.content.decode('utf-8'))
